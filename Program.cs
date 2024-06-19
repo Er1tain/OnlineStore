e12e01d720dfa887cbff8b08951850803ffa65a5
unknown <sanyapridava@mail.ru>
2024-06-19 22:03:10 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+namespace Csharp;
+
+static class Program
+{
+    
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }    
+}
