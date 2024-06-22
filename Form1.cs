a27916c7fe0741b5e231cc18e6903f46f90a3f78
Alexander Pridava <sanyapridava@mail.ru>
2024-06-22 10:11:46 +0300

    Add button for adding products

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Windows.Forms;
 
 namespace OnlineStore;
@@ -6,13 +7,20 @@ public partial class Form1 : Form
 {
     private System.ComponentModel.IContainer components = null;
 
-    private System.Windows.Forms.Button ProductsButton;
-    private System.Windows.Forms.Button ClientsButton;
-    private System.Windows.Forms.Button InvoiceButton;
-    private System.Windows.Forms.Button ReportButton;
+    private System.Windows.Forms.Button ProductsButton = new System.Windows.Forms.Button();
+    private System.Windows.Forms.Button ClientsButton = new System.Windows.Forms.Button();
+    private System.Windows.Forms.Button InvoiceButton = new System.Windows.Forms.Button();
+    private System.Windows.Forms.Button ReportButton = new System.Windows.Forms.Button();
 
     private void InitializeComponent()
     {
+        ProductsButton.Location = new System.Drawing.Point(12, 12);
+        ProductsButton.Name = "ProductsButton";
+        ProductsButton.Size = new System.Drawing.Size(200, 50);
+        ProductsButton.TabIndex = 0;
+        ProductsButton.Text = "Товары";
+        Controls.Add(ProductsButton);
+        
         this.components = new System.ComponentModel.Container();
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         this.ClientSize = new System.Drawing.Size(800, 450);
