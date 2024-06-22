4e63dd4e92f99007daa057ddd9767103881dda0c
Alexander Pridava <sanyapridava@mail.ru>
2024-06-22 10:16:25 +0300

    Add button for viewer list clients

--- Form1.cs
+++ Form1.cs
@@ -14,12 +14,22 @@ public partial class Form1 : Form
 
     private void InitializeComponent()
     {
+        //Кнопка для выбора приобретаемых товаров
         ProductsButton.Location = new System.Drawing.Point(12, 12);
         ProductsButton.Name = "ProductsButton";
         ProductsButton.Size = new System.Drawing.Size(200, 50);
         ProductsButton.TabIndex = 0;
         ProductsButton.Text = "Товары";
         Controls.Add(ProductsButton);
+
+        //Кнопка для отображения списка клиентов
+        ClientsButton.Location = new System.Drawing.Point(12, 68);
+        ClientsButton.Name = "ProductsButton";
+        ClientsButton.Size = new System.Drawing.Size(200, 50);
+        ClientsButton.TabIndex = 1;
+        ClientsButton.Text = "Клиенты";
+        Controls.Add(ClientsButton);
+
         
         this.components = new System.ComponentModel.Container();
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
